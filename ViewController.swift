f896583ea3184f35d0f7d6e1f8262d6848404ce6
felixvdl <vandelanottefelix@gmail.com>
2017-05-18 18:45:38 -0400

    complete

--- ViewController.swift
+++ ViewController.swift
@@ -10,8 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textDisplay: UILabel!
+    
+    @IBAction func helloWorld(_ sender: Any) {
+        textDisplay.text = "hello world!"
+    }
+    
+    @IBAction func goodbyeWorld(_ sender: Any) {
+        textDisplay.text = "goodbye world!"
+    }
+    
+    @IBAction func unicorn(_ sender: Any) {
+        textDisplay.text = "Unicorn"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
